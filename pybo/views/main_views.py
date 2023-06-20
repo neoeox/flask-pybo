2bc55f6dfa3f5b1995027ee1ff665b1bb7d3772c
jon <neoeox@naver.com>
2023-06-20 13:37:03 +0900

    직접 로그 출력하도록 설정하기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint(name='main', import_name=__name__, url_prefix='/')
@@ -11,6 +11,7 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    3 / 0  # 강제로 오류발생
+    # 3 / 0  # 강제로 오류발생
+    current_app.logger.info("INFO 레벨로 출력")
     return redirect(url_for('question._list'))
 
